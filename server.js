dd734012d2606bd60dcdd49dd7db922ca23066f0
Kevin <brackkevin@gmail.com>
2019-03-21 14:36:31 -0500

    initialized server.js set up link to routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// add cors
+// const cors = require("cors");
+
+const configureRoutes = require("./config/routes");
+
+const server = express();
+server.use(express.json());
+
+// uncomment after adding cors
+// server.use(cors());
+
+configureRoutes(server);
+
+module.exports = {
+  server
+};
