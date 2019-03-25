95face7ce7d1b8081feac8b399782e4c3934394e
Kevin <brackkevin@gmail.com>
2019-03-25 17:32:53 -0500

    Add production preset with env vars

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,27 @@ module.exports = {
         },
 
     },
+    production: {
+
+        client: 'mysql',
+
+        connection: {
+            host : `${process.env.MYSQL_HOST}`,
+            user : `${process.env.MYSQL_USER}`,
+            password : `${process.env.MYSQL_PASS}`,
+            database : `${process.env.MYSQL_DB}`
+        },
+
+        useNullAsDefault: true,
+
+        migrations: {
+            directory: './data/migrations'
+        },
+        seeds: {
+            directory: './data/productionSeeds'
+        },
+
+    }
 
 };
 
