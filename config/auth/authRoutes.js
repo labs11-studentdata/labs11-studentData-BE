fdefc31821270acededc8ca1fc3d8f3a8ed345bf
leianne louis <leilouis112@gmail.com>
2019-03-27 17:25:37 -0400

    added measure to check if the user is new or returning

--- config/auth/authRoutes.js
+++ config/auth/authRoutes.js
@@ -1,22 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express();
-const passport = require('passport');
-const jwt = require('./tokenService')
-const querystring = require('querystring');
+const passport = require("passport");
+const jwt = require("./tokenService");
+const querystring = require("querystring");
 // authentication with google
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-}));
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
 
-// callback for google redirect 
-router.get('/google/redirect', passport.authenticate('google'),  (req, res) => {
-    const token =  jwt.generateToken(req.user);
-    const user = req.user
-    const query = querystring.stringify({
-        "token": token
-    })
-    res.redirect(process.env.REACT_ROOT + '/' + query);
-    console.log(token)
-})
+// callback for google redirect
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  const token = jwt.generateToken(req.user);
+  const user = req.user;
+  const query = querystring.stringify({token: token});
+  const createdAt = user.created_at;
+  const date = Date(createdAt)
+  var isoString =  createdAt.slice(0, 10) + "T" + createdAt.substring(createdAt.length + 1, createdAt.indexOf(' ')).slice(1) + "Z"
+  if (Math.floor((new Date() - new Date(isoString)) / 60000) < 2) {
+    // NEW USER -- redirect to onboarding page
+    res.redirect(process.env.REACT_ROOT + "/" + query);
+    console.log(token);
+  } else {
+    // RETURNING USER -- check find user type and send back to client
+    console.log('old bitch')
+
+  }
+  
+});
 
 module.exports = router;
