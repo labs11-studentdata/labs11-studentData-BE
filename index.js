414ed5c318e1fdf2b5f2b89970da65687f1bf710
leianne louis <leilouis112@gmail.com>
2019-03-27 10:16:51 -0400

    fixed merge conflicts, auth working local

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 require('dotenv').config()
 const { server } = require("./api/server");
-const fs = require('fs');
-const https = require('https');
 // const express = require("express");
 const PORT = process.env.PORT || 9000;
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, server).listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
