14e3e2ac7ff3b8af991c1958f44fd8ca3264489f
spinon187 <spinon187@gmail.com>
2019-04-02 10:41:32 -0500

    first attempt at stripe endpoint

--- config/stripe/stripe.js
+++ config/stripe/stripe.js
@@ -3,19 +3,13 @@ const server = express();
 const key = process.env.STRIPE_SECRET_KEY;
 const stripe = require('stripe')(key);
 
-// function pay(email, amount){
-
-// }
-
 server.post('/charge', async (req, res) => {
   try {
     let {status} = await stripe.charges.create({
       amount: 2000,
       currency: "usd",
-      description: "An example charge",
       source: req.body
     });
-
     res.json({status});
   } catch (err) {
     res.status(500).end();
